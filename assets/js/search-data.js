00d653b13bedf987680a2463a55e825069499867
otroshi <72525669+otroshi@users.noreply.github.com>
2025-05-10 11:38:06 +0000

    Deploying to gh-pages from @ foundgen-bio/iccv2025@581fc51b930951c63e693486219ab9bffec9f1e6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -384,16 +384,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/iccv2025/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/iccv2025/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-call-for-paper-is-out",
+          title: 'Call for paper is out!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
